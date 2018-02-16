91b47a73736247bc0f10e56e69aaad782af9e50a
JanHav <haverbekej@gmail.com>
2018-02-16 09:29:20 +0100

    Extra comments

--- HelloGithub/HelloGithub.ino
+++ HelloGithub/HelloGithub.ino
@@ -26,17 +26,7 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  delay (1000); //Kijken of comments leesbaar zijn op github
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(4000);                       // wait for a second
-   delay(4000);  
-   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(4000);    
-}
+
--- HelloGithub/Test.ino
+++ HelloGithub/Test.ino
@@ -8,5 +8,5 @@ void loop() {
    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(2000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(4000);    
+  delay(4000);   //Hier wacht het programma 4 seconden 
 }
